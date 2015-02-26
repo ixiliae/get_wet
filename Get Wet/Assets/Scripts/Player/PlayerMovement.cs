f7d5936bfc249264325cc39e21f8f2bb194747d4
ixiss78 <maxence.duchet@gmail.Com>
2015-02-26 18:20:45 +0100

    Mousecamera
    la camera suit la souris. Il faut synchroniser les mouvements du
    personnage

--- Get Wet/Assets/Scripts/Player/PlayerMovement.cs
+++ Get Wet/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody playerRigidbody;
     int floorMask;
     float camRaylength = 100f;
+    public float rotateSpeed = 5;
+    Vector3 offset;
 
     void Awake()
     {
@@ -22,27 +24,25 @@ public class PlayerMovement : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
 
         Move(h, v);
-        Turning();
     }
     void Move(float h, float v)
     {
+       
+        float x = rotateSpeed * Input.GetAxis ("Mouse X");
+        // Rotation around the Euler angles
+        float pitch = -Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+        float yaw = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        float roll = -Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+        Vector3 rotation = new Vector3(pitch, yaw, roll);
+        rotation = Vector3.Lerp(Vector3.zero, rotation, Time.deltaTime * rotateSpeed); // Smooth...
+        // PHYSICS - nah
+        //Vector3 rotation = new Vector3(roll, yaw, pitch); // rigidbody rotation wants the angles in a different order...?
+        //transform.rigidbody.AddTorque(rotation);
+        transform.Rotate(rotation);
         movement.Set(h, 0f, v);
         movement = movement.normalized * Speed * Time.deltaTime;
         playerRigidbody.MovePosition(transform.position + movement);
     }
-    void Turning()
-    {
 
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit floorHit;
-
-        if (Physics.Raycast(camRay, out floorHit, camRaylength, floorMask))
-        {
-            Vector3 playerToMouse = floorHit.point - transform.position;
-            playerToMouse.y = 0f;
-            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-            playerRigidbody.MoveRotation(newRotation);
-        }
-    }
 
 }
---
+++ Get Wet/Assets/Thirpersoncamera.js
@@ -0,0 +1,15 @@
+﻿     var Damping = 12.0;
+    var Player : Transform;
+    var Height : float = 13.0;
+    var Offset : float = 0.0;
+    private var Center : Vector3;
+    var ViewDistance : float = 3.0;
+    function Update ()
+    {
+    var mousePos = Input.mousePosition;
+    mousePos.z = ViewDistance;
+    var CursorPosition : Vector3 = Camera.main.ScreenToWorldPoint(mousePos);
+    var PlayerPosition = Player.position;
+    Center = Vector3((PlayerPosition.x + CursorPosition.x) / 2, PlayerPosition.y, (PlayerPosition.z + CursorPosition.z) / 2);
+    transform.position = Vector3.Lerp(transform.position, Center + Vector3(0,Height,Offset), Time.deltaTime * Damping);
+    }
