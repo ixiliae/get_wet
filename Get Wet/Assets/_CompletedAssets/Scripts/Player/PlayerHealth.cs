29cf1e1a89955d341ba1393d3e6ef843ad7b908b
ixiss78 <maxence.duchet@gmail.Com>
2015-03-02 21:07:32 +0100

    health points v0.1
    Variable cree, anim aussi

--- Get Wet/Assets/_CompletedAssets/Scripts/Player/PlayerHealth.cs
+++ Get Wet/Assets/_CompletedAssets/Scripts/Player/PlayerHealth.cs
@@ -9,8 +9,6 @@ namespace CompleteProject
         public int startingHealth = 100;                            // The amount of health the player starts the game with.
         public int currentHealth;                                   // The current health the player has.
         public Slider healthSlider;                                 // Reference to the UI's health bar.
-        public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
-        public AudioClip deathClip;                                 // The audio clip to play when the player dies.
         public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
         public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
 
@@ -26,8 +24,6 @@ namespace CompleteProject
         void Awake ()
         {
             // Setting up the references.
-            anim = GetComponent <Animator> ();
-            playerAudio = GetComponent <AudioSource> ();
             playerMovement = GetComponent <PlayerMovement> ();
             playerShooting = GetComponentInChildren <PlayerShooting> ();
 
@@ -38,20 +34,6 @@ namespace CompleteProject
 
         void Update ()
         {
-            // If the player has just been damaged...
-            if(damaged)
-            {
-                // ... set the colour of the damageImage to the flash colour.
-                damageImage.color = flashColour;
-            }
-            // Otherwise...
-            else
-            {
-                // ... transition the colour back to clear.
-                damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-            }
-
-            // Reset the damaged flag.
             damaged = false;
         }
 
@@ -67,8 +49,6 @@ namespace CompleteProject
             // Set the health bar's value to the current health.
             healthSlider.value = currentHealth;
 
-            // Play the hurt sound effect.
-            playerAudio.Play ();
 
             // If the player has lost all it's health and the death flag hasn't been set yet...
             if(currentHealth <= 0 && !isDead)
@@ -84,15 +64,8 @@ namespace CompleteProject
             // Set the death flag so this function won't be called again.
             isDead = true;
 
-            // Turn off any remaining shooting effects.
-            playerShooting.DisableEffects ();
-
             // Tell the animator that the player is dead.
-            anim.SetTrigger ("Die");
-
-            // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
-            playerAudio.clip = deathClip;
-            playerAudio.Play ();
+            animation.Play("Tpause");
 
             // Turn off the movement and shooting scripts.
             playerMovement.enabled = false;
