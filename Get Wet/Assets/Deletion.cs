a4e45820007dc82a80b1498edc0dde69d716713e
ixiss78 <maxence.duchet@gmail.Com>
2015-03-03 23:12:36 +0100

    Shooting mechanics v1.0
    Need to adjust angle/position

---
+++ Get Wet/Assets/Deletion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deletion : MonoBehaviour {
+
+	// Use this for initialization
+    public float bulletLife = 5.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Destroy(gameObject, bulletLife);
+	}
+}
