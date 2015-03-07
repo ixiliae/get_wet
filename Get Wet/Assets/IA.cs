a657a7403380cf4a1e28d979c96da36896c39596
Ixiss <maxence.duchet@gmail.com>
2015-03-07 14:40:11 +0100

    added death + npc village

--- Get Wet/Assets/IA.cs
+++ Get Wet/Assets/IA.cs
@@ -7,17 +7,18 @@ public class IA : MonoBehaviour {
 	float AIspeed = 3.5f;
 	bool AImoving = true;
 	float AIrotate = 90f;
+	PlayerHealth p;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
-
+		p = player.GetComponent <PlayerHealth>();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		if (Vector3.Distance (player.transform.position, transform.position) < 30) {
+		if ((Vector3.Distance(player.transform.position, transform.position)< 30)&& (Vector3.Distance(player.transform.position, transform.position)> 3)){
 			transform.LookAt (leader);
 			transform.position += transform.forward * AIspeed * Time.deltaTime;
 			transform.FindChild("baseMale").animation.Play("walk");
--- Get Wet/Assets/Scripts/Camera/MouseAimCamera.cs
+++ Get Wet/Assets/Scripts/Camera/MouseAimCamera.cs
@@ -6,21 +6,25 @@ public class MouseAimCamera : MonoBehaviour
     public GameObject target;
     public float rotateSpeed = 5;
     Vector3 offset;
+	PlayerHealth p;
 
     void Start()
     {
         offset = target.transform.position - transform.position;
+		p = gameObject.GetComponentInParent<PlayerHealth>();
     }
 
     void LateUpdate()
     {
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        target.transform.Rotate(0, horizontal, 0); 
+		if (p.isDead == false) {
+			float horizontal = Input.GetAxis ("Mouse X") * rotateSpeed;
+			target.transform.Rotate (0, horizontal, 0); 
 
-        float desiredAngle = target.transform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-        transform.position = target.transform.position - (rotation * offset);
+			float desiredAngle = target.transform.eulerAngles.y;
+			Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+			transform.position = target.transform.position - (rotation * offset);
 
-        transform.LookAt(target.transform);
+			transform.LookAt (target.transform);
+		}
     }
 }
