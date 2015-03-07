43f78a7febec89de29da46211c29644b462c04a4
Ixiss <maxence.duchet@gmail.com>
2015-03-07 18:10:18 +0100

    Ia waypoints
    fini IA

---
+++ Get Wet/Assets/IAnav.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAnav : MonoBehaviour
+{
+
+	public GameObject player;
+	public Transform leader;
+	float AIspeed = 3.5f;
+	bool AImoving = true;
+	float AIrotate = 90f;
+	public Transform waypoint1;
+	public Transform waypoint2;
+	public Transform waypoint3;
+	int w;
+	Vector3[] waypoints;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");		
+		waypoints = new Vector3[] {waypoint1.position, waypoint2.position ,waypoint3.position};
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = waypoints[0]; 
+		transform.FindChild ("baseMale").animation.Play ("walk");
+	}
+	void Update () {
+		
+		if (false && (Vector3.Distance (player.transform.position, transform.position) < 30) && (Vector3.Distance (player.transform.position, transform.position) > 3)) {
+			GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
+		} else {
+			if (Vector3.Distance(transform.position, waypoints[w]) < 1)
+			{
+				GetComponent<NavMeshAgent>().SetDestination(waypoints[w = (w + 1) % waypoints.Length]);
+
+
+			}
+
+		}
+
+					}
+
+}
---
+++ Get Wet/Assets/IAstill.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAstill : MonoBehaviour {
+	public GameObject player;
+	public Transform leader;
+	float AIspeed = 3.5f;
+	bool AImoving = true;
+	float AIrotate = 90f;
+	PlayerHealth p;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		p = player.GetComponent <PlayerHealth>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if ((Vector3.Distance(player.transform.position, transform.position)< 30)&& (Vector3.Distance(player.transform.position, transform.position)> 3)){
+			transform.LookAt (leader);
+			transform.position += transform.forward * AIspeed * Time.deltaTime;
+			transform.FindChild("baseMale").animation.Play("walk");
+		} else
+			transform.FindChild("baseMale").animation.Play("idle");
+		
+	}
+}
