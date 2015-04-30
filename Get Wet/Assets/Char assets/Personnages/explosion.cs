6512ef6e8cfd57cf731585f5383a0e93235d4101
Matthieua1 <matthieu.a1@hotmail.fr>
2015-04-30 14:27:36 +0200

    PLs Work
    pls god pls

---
+++ Get Wet/Assets/Char assets/Personnages/explosion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosion : MonoBehaviour {
+	
+	// Use this for initialization
+	public float bulletLife = 10.0f;
+	private GameObject player;
+	PlayerHealth p;
+	EnemyHealth e;
+	Object parent;
+	public GameObject body;
+	public Transform prefab;
+	void Start()
+	{
+		parent = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Destroy(gameObject, bulletLife);
+		
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		Instantiate(prefab, col.transform.position, Quaternion.identity);
+		Destroy (gameObject, 0);
+		
+	}
+
+}
