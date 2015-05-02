6facfe0fd65d718e2467aa46958b4a307bbfa073
ixiss78 <maxence.duchet@gmail.Com>
2015-05-02 04:50:30 +0200

    Turret v1

---
+++ Get Wet/Assets/Turrets Pack/turretDamage.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turretDamage : MonoBehaviour {
+	
+	// Use this for initialization
+	public float bulletLife = 10.0f;
+	private GameObject player;
+	PlayerHealth p;
+	EnemyHealth e;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Destroy(gameObject, bulletLife);
+		
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		player = col.gameObject;
+		if ((col.gameObject.name == "whitinola") || (col.gameObject.name == "Blackinola"))
+		{
+			p = player.GetComponent<PlayerHealth>();
+			p.TakeDamage(1);
+			Destroy(gameObject, 0);
+			PlayerManager.Instance.AddHealth(0, -1);
+		}
+		if (col.gameObject.name == "Enemy")
+		{
+			e = player.GetComponent<EnemyHealth>();
+			e.TakeDamage(1);
+			Destroy(gameObject, 0);
+		}
+		
+	}
+}
---
+++ Get Wet/Assets/Turrets Pack/turretShoot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turretShoot : MonoBehaviour
+{
+	public Rigidbody projectile;
+	public float speed = 20;
+
+	public GameObject player;
+	public Transform leader;
+	
+	
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		
+	}
+	// Update is called once per frame
+	void Update()
+	{
+			if (Vector3.Distance (player.transform.position, transform.position) < 20) {
+				transform.parent.LookAt(leader);
+				Rigidbody instantiatedProjectile = Instantiate (projectile, transform.position, transform.rotation) as Rigidbody;
+				instantiatedProjectile.velocity = transform.TransformDirection (new Vector3 (0, 0, speed));
+				
+			
+		}
+
+	}
+	
+}		
