bf3c7e0b39cd1f8ad322b78823c5a6d8902ae503
ixiss78 <maxence.duchet@gmail.Com>
2015-06-18 12:21:13 +0200

    .meta et fix characters
    tout est dans le titre

---
+++ Get Wet/Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+
+
+    new NetworkView networkView;
+    public PlayerMovement playerMovement;
+
+    void Start()
+    {
+        networkView = GetComponent<NetworkView>();
+    }
+
+    void Update()
+    {
+        float move_h = Input.GetAxis("Horizontal");
+        float move_v = Input.GetAxis("Vertical");
+
+
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+            playerMovement.forward(move_v, move_h);
+
+
+        if (Input.GetButton("Fire1"))
+        {
+        }
+     }
+    }
---
+++ Get Wet/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    const float sprintSpeed = 15f;
+    const float sneakSpeed = 5f;
+    const float rotationSpeed = 200f;
+    public float speed = 6.0F;
+    public float jumpSpeed = 16.0F;
+    public float gravity = 20.0F;
+    Animator animator;
+    new NetworkView networkView;
+    private Vector3 moveDirection = Vector3.zero;
+    PlayerHealth p;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        networkView = GetComponent<NetworkView>();
+
+    }
+
+    void Move(float v, float h, string anim_f)
+    {
+        
+
+        if (networkView != null)
+            networkView.RPC("PlayAnimation", RPCMode.Others);
+
+
+            CharacterController controller = GetComponent<CharacterController>();
+
+                moveDirection = new Vector3(v, 0, h);
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+                if (Input.GetButton("Jump"))
+                    moveDirection.y = jumpSpeed;
+           
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+    
+
+    public void forward(float amount, float amount2)
+    {
+        this.transform.Translate(0, 0, amount * speed * Time.deltaTime);
+            Move(amount, amount2, "run");
+
+    }
+
+}
