69d64ae2f3ee4f99fc061b15bc48ef198be94edf
ixiss78 <maxence.duchet@gmail.Com>
2015-06-20 01:30:11 +0200

    Capture the flag v0.5

---
+++ Get Wet/Assets/Scripts/CTF/GetFlag.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetFlag : MonoBehaviour {
+
+	// Use this for initialization
+    private GameObject flagStealer;
+    Vector3 flagPos;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (flagStealer != null)
+        {
+            flagPos = flagStealer.transform.position;
+            flagPos.y += 5f;
+            transform.position = flagPos;
+        }
+	
+	}
+
+    void OnTriggerEnter(Collider player)
+    {
+        
+        if (player.transform.gameObject.tag == "Player")
+             flagStealer = player.gameObject;
+        if (player.transform.gameObject.tag == "Finish")
+            Destroy(this.gameObject);
+
+             
+    
+    
+    }
+}
---
+++ Get Wet/Assets/Scripts/CTF/RotateFinish.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateFinish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, 150*Time.deltaTime, 0);
+	
+	}
+}
