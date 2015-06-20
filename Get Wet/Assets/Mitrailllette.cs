59d644a3c6d3fec300b12d43118b6f7a146e0d03
ixiss78 <maxence.duchet@gmail.Com>
2015-06-20 19:11:26 +0200

    Much swag much wow

--- Get Wet/Assets/Mitrailllette.cs
+++ Get Wet/Assets/Mitrailllette.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Mitrailllette : MonoBehaviour {
+public class Mitrailllette : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    public float bulletLife = 10.0f;
+    private GameObject player;
+    PlayerHealth p;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Destroy(gameObject, bulletLife);
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        player = col.gameObject;
+
+        p = col.gameObject.GetComponent<PlayerHealth>();
+        p.TakeDamage(5);
+        Destroy(gameObject, 0);
+    }
 }
